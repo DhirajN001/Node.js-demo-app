30ccb6f16b6261b5c55b5f2b8f8f20d0bd4bf956
Dhiraj  Nimbalkar <158476919+DhirajN001@users.noreply.github.com>
2025-09-14 10:18:15 +0530

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-
 export default function App() {
   return (
-  function App() {
-  return (
-  <div style={{fontFamily:'Arial',padding:24}}>
+    <div style={{ fontFamily: 'Arial', padding: 24 }}>
       <h1>AWS DevOps Engineer – Demo</h1>
       <p>This is a minimal React app deployed via CI/CD to S3 + CloudFront.</p>
-      <p>Demo login: <b>hire-me@anshumat.org</b> / <b>HireMe@2025!</b></p>
+      <p>
+        Demo login: <b>hire-me@anshumat.org</b> / <b>HireMe@2025!</b>
+      </p>
     </div>
   );
 }
